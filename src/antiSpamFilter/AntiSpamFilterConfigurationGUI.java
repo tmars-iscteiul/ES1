c8a9c76ce0ae3b943f6146782633a68c65d6e398
Tiago Rodrigues <tmars@iscte-iul.pt>
2017-11-06 18:31:38 +0000

    implementação de objetos na janela de configuração terminada v1.4

---
+++ src/antiSpamFilter/AntiSpamFilterConfigurationGUI.java
@@ -0,0 +1,202 @@
+package antiSpamFilter;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+
+import antiSpamFilter.AntiSpamFilterStyles.*;
+
+public class AntiSpamFilterConfigurationGUI {
+	private JFrame antiSpamFilterFrame = new JFrame("AntiSpamFilter Configuration v1.0");
+	
+	private final int WINDOW_HSIZE = 500;
+	private final int WINDOW_VSIZE = 500;
+	private final int COMPONENT_GAP = 10;
+	private final int COMPONENT_MAX_WIDTH = WINDOW_HSIZE-(2*COMPONENT_GAP);
+	private final int COMPONENT_MAX_HEIGHT = WINDOW_VSIZE-(2*COMPONENT_GAP);
+	
+	private final double MIN_RULE = -5.0;
+	private final double MAX_RULE = 5.0;
+	private final double INT_RULE = 0.1;
+	private final double INIC_RULE = 0.0;
+	
+	//Panels initiation
+	APanel ruleslistPanel, principalPanel, searchPanel, configurationPanel, inputPanel, applyPanel,
+		testsPanel, conclusionPanel;
+
+	public AntiSpamFilterConfigurationGUI() {
+		//Dimension and position of the window
+		antiSpamFilterFrame.setSize(WINDOW_HSIZE, WINDOW_VSIZE);
+		antiSpamFilterFrame.setLocationRelativeTo(null);
+		antiSpamFilterFrame.setLocation(
+				(int)antiSpamFilterFrame.getLocation().getX()+80,
+				(int)antiSpamFilterFrame.getLocation().getY()+20);
+		
+		setRulesListPanel();
+		setMainConfigurationPanel();		
+		setSearchPanel();
+		setRuleConfigurationPanel();		
+		setInputPanel();
+		setApplyTestButtons();		
+		setTestResultsPanel();
+		
+		configurationPanel.setLayout(new BorderLayout(COMPONENT_GAP,COMPONENT_GAP));
+		configurationPanel.setPreferredSize(new Dimension(200,300));
+		configurationPanel.setBorder(
+				BorderFactory.createEmptyBorder(COMPONENT_GAP, 0, COMPONENT_GAP, 0));
+		
+		configurationPanel.add(inputPanel, BorderLayout.PAGE_START);
+		configurationPanel.add(applyPanel, BorderLayout.CENTER);
+		configurationPanel.add(testsPanel, BorderLayout.PAGE_END);
+		
+		//Implementation of back and save buttons
+		conclusionPanel = new AntiSpamFilterStyles().new APanel();
+		conclusionPanel.setPreferredSize(new Dimension(COMPONENT_MAX_WIDTH,50));
+		setConclusaoPanel();
+		
+		//Inclusion of the panels in the application window
+		principalPanel.add(searchPanel, BorderLayout.NORTH);
+		principalPanel.add(configurationPanel, BorderLayout.CENTER);
+		principalPanel.add(conclusionPanel, BorderLayout.PAGE_END);
+
+		
+		//Implementação do ambiente
+		antiSpamFilterFrame.setLayout(new BorderLayout(2,2));
+
+		antiSpamFilterFrame.getContentPane().add(ruleslistPanel,BorderLayout.WEST);
+		antiSpamFilterFrame.getContentPane().add(principalPanel,BorderLayout.CENTER);
+
+		antiSpamFilterFrame.setIconImage(Toolkit.getDefaultToolkit().getImage("Icon.png"));
+		antiSpamFilterFrame.setResizable(false);
+		antiSpamFilterFrame.setVisible(true);
+	}
+
+	private void setTestResultsPanel() {
+		//Implements the test results window
+		testsPanel.setLayout(new BorderLayout(COMPONENT_GAP,COMPONENT_GAP));
+		ALabel testsLabel = new AntiSpamFilterStyles().new ALabel("Test results");
+		String[] testResults = {"FP: 38 in 2000 mails", "FN: 45 in 2000 mails", "Compare results: improved", "Efficiency: 7%", "Tip: save the optimization values"};
+		AList testResultsList = new AntiSpamFilterStyles().new AList(testResults);
+		testResultsList.setFont(new Font("Arial", Font.PLAIN, 16));
+		testsPanel.add(testsLabel, BorderLayout.PAGE_START);
+		testsPanel.add(testResultsList, BorderLayout.CENTER);
+	}
+
+	private void setApplyTestButtons() {
+		//Implementation of apply and test buttons
+		applyPanel = new AntiSpamFilterStyles().new APanel();
+		applyPanel.setPreferredSize(new Dimension(COMPONENT_MAX_WIDTH,40));
+		setApplyPanel();
+	}
+
+	private void setInputPanel() {
+		//Implementation of input window
+		inputPanel.setLayout(new BorderLayout(COMPONENT_GAP,COMPONENT_GAP));
+		ALabel configurationLabel = new AntiSpamFilterStyles().new ALabel("Configuration Panel");
+		ALabel weightLabel = new AntiSpamFilterStyles().new ALabel("Rule weight:", AntiSpamFilterStyles.TXT_SMALL);
+
+		SpinnerModel spinnerModel = new SpinnerNumberModel(INIC_RULE, MIN_RULE, MAX_RULE, INT_RULE);
+		ASpinner spinner = new AntiSpamFilterStyles().new ASpinner(spinnerModel);
+
+		inputPanel.add(configurationLabel, BorderLayout.PAGE_START);
+		inputPanel.add(weightLabel, BorderLayout.LINE_START);
+		inputPanel.add(spinner, BorderLayout.CENTER);
+	}
+
+	private void setRuleConfigurationPanel() {
+		//Implementation of rule configuration window
+		configurationPanel = new AntiSpamFilterStyles().new APanel();
+		inputPanel = new AntiSpamFilterStyles().new APanel();
+		testsPanel = new AntiSpamFilterStyles().new APanel();
+	}
+
+	private void setSearchPanel() {
+		//Implementation of search window
+		searchPanel = new AntiSpamFilterStyles().new APanel();
+		ATextField searchField = new AntiSpamFilterStyles().new ATextField("");
+		searchField.setPlaceholder("Search field");
+
+		AButton clearSearchButton = 
+				new AntiSpamFilterStyles().
+				new AButton("Clear search", AntiSpamFilterStyles.BTN_DEFAULT);
+
+		searchPanel.setLayout(new BorderLayout(COMPONENT_GAP,5));
+		searchPanel.setPreferredSize(new Dimension(200,75));
+		searchPanel.add(searchField,BorderLayout.NORTH);
+		searchPanel.add(clearSearchButton, BorderLayout.CENTER);
+	}
+
+	private void setMainConfigurationPanel() {
+		//Implementation of main configuration window
+		principalPanel = new AntiSpamFilterStyles().new APanel();
+		principalPanel.setLayout(new BorderLayout(COMPONENT_GAP,COMPONENT_GAP));
+		principalPanel.setBorder(
+				BorderFactory.createEmptyBorder(COMPONENT_GAP,COMPONENT_GAP,COMPONENT_GAP,COMPONENT_GAP));
+	}
+
+	private void setRulesListPanel() {
+		//Implementation of rules list window
+		ruleslistPanel = new AntiSpamFilterStyles().new APanel();
+		ruleslistPanel.setPreferredSize(new Dimension(200,COMPONENT_MAX_HEIGHT));
+		ruleslistPanel.setLayout(new BorderLayout(10,10));
+		ruleslistPanel.setBorder(
+				BorderFactory.createEmptyBorder(COMPONENT_GAP,COMPONENT_GAP,COMPONENT_GAP,COMPONENT_GAP));
+
+		ALabel spamLabel = new AntiSpamFilterStyles().new ALabel("Rules List");
+		ruleslistPanel.add(spamLabel, BorderLayout.PAGE_START);
+		
+		String[] rList = {"BAYES_00 (4.1)","FREEMAIL_FROM (1.8)","RDNS_NONE (-2.6)"};
+		AList rulesList = new AntiSpamFilterStyles().new AList(rList);
+		
+		ruleslistPanel.add(rulesList, BorderLayout.CENTER);
+	}
+
+	private void setApplyPanel() {
+		APanel buttonPanel = setButtonPanel();
+
+		AButton applyButton = 
+				new AntiSpamFilterStyles().
+				new AButton("< Apply", AntiSpamFilterStyles.BTN_DEFAULT);
+		
+		AButton testButton = 
+				new AntiSpamFilterStyles().
+				new AButton("Test configuration", AntiSpamFilterStyles.BTN_DEFAULT);
+
+		buttonPanel.add(applyButton, BorderLayout.LINE_START);
+		buttonPanel.add(testButton, BorderLayout.CENTER);
+		applyPanel.add(buttonPanel);
+	}
+	
+	private void setConclusaoPanel() {
+		APanel buttonPanel = setButtonPanel();
+
+		AButton backButton = 
+				new AntiSpamFilterStyles().
+				new AButton("Back", AntiSpamFilterStyles.BTN_DEFAULT);
+		
+		AButton saveButton = 
+				new AntiSpamFilterStyles().
+				new AButton("Save configuration", AntiSpamFilterStyles.BTN_DEFAULT);
+		
+		buttonPanel.add(backButton, BorderLayout.LINE_START);
+		buttonPanel.add(saveButton, BorderLayout.CENTER);
+		conclusionPanel.add(buttonPanel);
+	}
+	
+	private APanel setButtonPanel() {
+		APanel buttonPanel = new AntiSpamFilterStyles().new APanel();
+		buttonPanel.setPreferredSize(new Dimension(270,40));
+		BorderLayout layout = new BorderLayout(COMPONENT_GAP,COMPONENT_GAP);
+		buttonPanel.setLayout(layout);
+		return buttonPanel;
+	}
+	
+	
+
+}
---
+++ src/antiSpamFilter/AntiSpamFilterStyles.java
@@ -0,0 +1,188 @@
+package antiSpamFilter;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpinnerModel;
+import javax.swing.JSpinner;
+
+public class AntiSpamFilterStyles {
+	
+	protected final static int BTN_DEFAULT = 1;
+	protected final static int BTN_SUCCESS = 2;
+	protected final static int BTN_DANGER = 3;
+	
+	protected final static int TXT_TITLE = 1;
+	protected final static int TXT_SMALL = 2;
+	
+	protected final static Color TEXTCOLOR = new Color(100,100,100);
+	protected final static Color DEFAULT = new Color(102,153,255);
+	protected final static Color DANGER = new Color(255,102,102);
+	protected final static Color SUCCESS = new Color(153,204,51);
+
+	//Style definition for the GUI objets:
+	//JPanel -> use APanel	
+	class APanel extends JPanel {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public APanel() {
+			super();
+			super.setBackground(Color.WHITE);
+		}
+	}	
+
+	//JLabel -> use ALabel		
+	class ALabel extends JLabel {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public ALabel(String string) {
+			super(string);
+			super.setFont(new Font("Arial", Font.PLAIN, 18));
+			super.setForeground(TEXTCOLOR);
+		}
+		
+		public ALabel(String string, int tipo) {
+			super(string);
+			if (tipo == TXT_TITLE) {super.setFont(new Font("Arial", Font.PLAIN, 18));}
+			else if (tipo == TXT_SMALL) {super.setFont(new Font("Arial", Font.PLAIN, 16));}
+			super.setForeground(TEXTCOLOR);
+		}
+	}
+
+	//JComboBox -> use AComboBox		
+	class AComboBox extends JComboBox<Object> {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public AComboBox(String[] string) {
+			super(string);
+			super.setFont(new Font("Arial", Font.PLAIN, 14));
+			super.setForeground(TEXTCOLOR);
+			super.setBackground(Color.WHITE);
+		}
+	}
+
+	//JButton -> use AButton			
+	class AButton extends JButton {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public AButton(String string, int tipo) {
+			super(string);
+			super.setFont(new Font("Arial", Font.PLAIN, 16));
+			super.setForeground(TEXTCOLOR);
+			if (tipo == BTN_DANGER) {
+				super.setBackground(DANGER);
+				super.setForeground(Color.WHITE);
+			}
+			else if (tipo == BTN_SUCCESS) {
+				super.setBackground(SUCCESS);
+				super.setForeground(Color.WHITE);
+			} 
+			else if (tipo == BTN_DEFAULT) {
+				super.setBackground(DEFAULT);
+				super.setForeground(Color.WHITE);
+			}
+		}
+	}
+
+	//JList -> use AList		
+	class AList extends JList<Object> {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public AList(DefaultListModel list) {
+			super(list);
+			super.setFont(new Font("Arial", Font.PLAIN, 14));
+			super.setForeground(TEXTCOLOR);
+		}
+		
+		public AList(String[] list) {
+			super(list);
+			super.setFont(new Font("Arial", Font.PLAIN, 14));
+			super.setForeground(TEXTCOLOR);
+
+		}
+	}
+	
+	//JTextField -> use ATextField
+	class ATextField extends JTextField {
+		
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private String placeholder;
+
+		public ATextField (String string) {
+			super(string);
+			super.setFont(new Font("Arial", Font.PLAIN, 16));
+			super.setForeground(TEXTCOLOR);
+			super.setPreferredSize(new Dimension(super.getWidth(),30));
+		}
+		
+		@Override
+	    protected void paintComponent(final Graphics pG) {
+	        super.paintComponent(pG);
+
+	        if (placeholder.length() == 0 || getText().length() > 0) {
+	            return;
+	        }
+
+	        final Graphics2D g = (Graphics2D) pG;
+	        g.setRenderingHint(
+	            RenderingHints.KEY_ANTIALIASING,
+	            RenderingHints.VALUE_ANTIALIAS_ON);
+	        g.setColor(getDisabledTextColor());
+	        g.drawString(placeholder, getInsets().left, pG.getFontMetrics()
+	            .getMaxAscent() + getInsets().top + 3);
+	    }
+
+	    public void setPlaceholder(final String s) {
+	        placeholder = s;
+	    }
+	}
+	
+	//JSpinner -> use ASpinner
+	class ASpinner extends JSpinner {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		
+		public ASpinner(SpinnerModel s) {
+			super(s);
+			super.setFont(new Font("Arial", Font.PLAIN, 16));
+			super.setForeground(TEXTCOLOR);
+			super.setPreferredSize(new Dimension(super.getWidth(),30));
+			JComponent editor = this.getEditor();
+			JSpinner.DefaultEditor spinnerEditor = (JSpinner.DefaultEditor) editor;
+			spinnerEditor.getTextField().setHorizontalAlignment(JTextField.CENTER);
+		}
+	}
+}
