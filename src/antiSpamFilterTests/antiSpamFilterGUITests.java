e1312be49491925046bde3a76dc0a7cb8ec02b4b
Rodolfo Arnaldo <rgnsa@iscte-iul.pt>
2017-11-11 17:04:22 +0000

    Testes inicio

---
+++ src/antiSpamFilterTests/antiSpamFilterGUITests.java
@@ -0,0 +1,180 @@
+/**
+ * 
+ */
+package antiSpamFilterTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import antiSpamFilter.AntiSpamFilterGUI;
+
+/**
+ * @author Rodolfo Arnaldo
+ *
+ */
+public class antiSpamFilterGUITests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getSerialversionuid()}.
+	 */
+	@Test
+	public void testGetSerialversionuid() {
+		AntiSpamFilterGUI anti = new AntiSpamFilterGUI();
+		assertEquals(1L, anti.getSerialversionuid());
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#AntiSpamFilterGUI()}.
+	 */
+	@Test
+	public void testAntiSpamFilterGUI() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#setEnable(boolean)}.
+	 */
+	@Test
+	public void testSetEnable() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getWINDOW_HSIZE()}.
+	 */
+	@Test
+	public void testGetWINDOW_HSIZE() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getWINDOW_VSIZE()}.
+	 */
+	@Test
+	public void testGetWINDOW_VSIZE() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getCOMPONENT_GAP()}.
+	 */
+	@Test
+	public void testGetCOMPONENT_GAP() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getCOMPONENT_MAX_WIDTH()}.
+	 */
+	@Test
+	public void testGetCOMPONENT_MAX_WIDTH() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getSPAM_FILENAME()}.
+	 */
+	@Test
+	public void testGetSPAM_FILENAME() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getSPAM_DIRECTORY()}.
+	 */
+	@Test
+	public void testGetSPAM_DIRECTORY() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getSPAM_FILE()}.
+	 */
+	@Test
+	public void testGetSPAM_FILE() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getHAM_FILENAME()}.
+	 */
+	@Test
+	public void testGetHAM_FILENAME() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getHAM_DIRECTORY()}.
+	 */
+	@Test
+	public void testGetHAM_DIRECTORY() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getHAM_FILE()}.
+	 */
+	@Test
+	public void testGetHAM_FILE() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getRULES_FILENAME()}.
+	 */
+	@Test
+	public void testGetRULES_FILENAME() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getRULES_DIRECTORY()}.
+	 */
+	@Test
+	public void testGetRULES_DIRECTORY() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link antiSpamFilter.AntiSpamFilterGUI#getRULES_FILE()}.
+	 */
+	@Test
+	public void testGetRULES_FILE() {
+		fail("Not yet implemented");
+	}
+
+	
+}
