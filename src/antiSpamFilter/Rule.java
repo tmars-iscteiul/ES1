ceb5b3944217c41aa1656570e65d75b6b8e418db
Catarina Carriço <Catarina Carrio@DESKTOP-TG7QKSL>
2017-11-20 17:15:26 +0000

    Iniciação de dois métodos na classe Rule apenas para o código não ficar com erro

--- src/antiSpamFilter/Rule.java
+++ src/antiSpamFilter/Rule.java
@@ -1,9 +1,21 @@
 package antiSpamFilter;
 
+import java.io.File;
+
 public class Rule {
 
 	public Rule(String string, double weight) {
 		// TODO Auto-generated constructor stub
+	}
+
+	public File getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
