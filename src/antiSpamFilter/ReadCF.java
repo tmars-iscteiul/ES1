5795cc7ad27e3f2b49db6fc80fea16138744337f
ricardo <rarde@iscte.pt>
2017-11-23 15:48:40 +0000

    Validação ficheiro rules.cf

--- src/antiSpamFilter/ReadCF.java
+++ src/antiSpamFilter/ReadCF.java
@@ -2,21 +2,39 @@ package antiSpamFilter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReadCF {
 
 	public static void read(File file) {
 
+		String line = null;
+		ArrayList<String> lineList = new ArrayList<String>();
+		boolean regraValida = false;
+
 		try {
 
 			Scanner scanner = new Scanner(file);
 
 			while (scanner.hasNextLine()) {
-				String line = scanner.nextLine();
-				System.out.println(line);
+
+				line = scanner.nextLine();
+				line.split(" ");
+				lineList.add(line);
+				String[] vetor = line.split(" ");
+
+
+				if (vetor.length == 1) 
+					regraValida = true;
+
+					
+				System.out.println("regra valida: " + line + "---->"+ regraValida);
+
+				regraValida = false;
 			}
-			// scanner.close();
+			
+			scanner.close();
 
 		} catch (FileNotFoundException e) {
 			e.getStackTrace();
