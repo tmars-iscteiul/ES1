9c7ac2c39cf41fe6c5519c0cd4f6d3daa44cab04
Tiago Rodrigues <tmars@iscte-iul.pt>
2017-11-26 15:52:42 +0000

    Criação de testes para a classe EmailStream

--- src/antiSpamFilter/tests/EmailStreamTest.java
+++ src/antiSpamFilter/tests/EmailStreamTest.java
@@ -2,13 +2,31 @@ package antiSpamFilter.tests;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import antiSpamFilter.emails.Email;
+import antiSpamFilter.emails.EmailStream;
+import antiSpamFilter.rules.Rule;
+import antiSpamFilter.rules.RuleStream;
 
 public class EmailStreamTest {
 
 	@Test
 	public final void testGetListOfEmailsFromFile() {
-		fail("Not yet implemented"); // TODO
+		ArrayList<Rule> listOfRules = new RuleStream(new File("Files/rules.cf")).getRuleStream();
+		
+		ArrayList<Email> listOfEmails1 = EmailStream.getListOfEmailsFromFile(
+				new File("Files/spam_test_valid.log"), listOfRules, Email.SPAM);
+		
+		ArrayList<Email> listOfEmails2 = EmailStream.getListOfEmailsFromFile(
+				new File("Files/test.log"), listOfRules, Email.SPAM);
+		
+		ArrayList<Email> listOfEmails3 = EmailStream.getListOfEmailsFromFile(
+				new File("Files/spam_test_invalid_rule.log"), listOfRules, Email.SPAM);
+				
 	}
 
 	@Test
--- src/antiSpamFilter/tests/ReadCFTest.java
+++ src/antiSpamFilter/tests/ReadCFTest.java
@@ -12,27 +12,27 @@ public class ReadCFTest {
 
 	@Test
 	public final void testValidRead() {
-		assertTrue(ReadCF.read(new File("Ficheiros/rules_test_valid.cf")));
+		assertTrue(ReadCF.read(new File("Files/rules_test_valid.cf")));
 	}
 	
 	@Test
 	public final void testInvalidDoubleRead() {
-		assertFalse(ReadCF.read(new File("Ficheiros/rules_test_invalid_double.cf")));
+		assertFalse(ReadCF.read(new File("Files/rules_test_invalid_double.cf")));
 	}
 	
 	@Test
 	public final void testInvalidFieldsRead() {
-		assertFalse(ReadCF.read(new File("Ficheiros/rules_test_invalid_fields.cf")));
+		assertFalse(ReadCF.read(new File("Files/rules_test_invalid_fields.cf")));
 	}
 	
 	@Test
 	public final void testInvalidWeightRead() {
-		assertFalse(ReadCF.read(new File("Ficheiros/rules_test_invalid_weight.cf")));
+		assertFalse(ReadCF.read(new File("Files/rules_test_invalid_weight.cf")));
 	}
 	
 	@Test
 	public final void testInvalidFile() {
-		assertFalse(ReadCF.read(new File("Ficheiros/test.cf")));
+		assertFalse(ReadCF.read(new File("Files/test.cf")));
 	}
 
 }
