7476aff3e68eaa840fe627144cfbeaec43eb7957
Tiago Rodrigues <tmars@iscte-iul.pt>
2017-11-30 21:26:29 +0000

    Implementação do JavaDoc do Manual Configuration

--- src/antiSpamFilter/AntiSpamFilterManualConfiguration.java
+++ src/antiSpamFilter/AntiSpamFilterManualConfiguration.java
@@ -5,8 +5,19 @@ import java.util.ArrayList;
 import antiSpamFilter.GUI.AntiSpamFilterConfigurationGUI;
 import antiSpamFilter.rules.Rule;
 
-public class AntiSpamFilterManualConfiguration {
+/**
+ * <p>AntiSpamFilterManualConfiguration - the rules configuration class</br>
+ * </br>
+ * The AntiSpamFilter Manual Configuration allows the user to manually change
+ * and test the list of rules. This class also creates the configuration GUI
+ * and interacts with it.</p>
+ *
+ * @author ES1-2017-LIGE-PL-102
+ */
 
+public class AntiSpamFilterManualConfiguration {
+	
+	//Launch of the global variables
 	AntiSpamFilterAutomaticConfiguration main;
 	AntiSpamFilterConfigurationGUI gui;
 	ArrayList<Rule> mainListOfRules, temporaryListOfRules;
@@ -19,6 +30,7 @@ public class AntiSpamFilterManualConfiguration {
 		gui = new AntiSpamFilterConfigurationGUI(this,false);
 	}
 
+	/**Creation of the main list and the temporary list of rules**/
 	public void startConfiguration() {
 		mainListOfRules = main.getListOfRules();
 		temporaryListOfRules = new ArrayList<Rule>();
@@ -28,6 +40,7 @@ public class AntiSpamFilterManualConfiguration {
 		
 		updateListOfNames();
 		
+		//See if the window was already created before
 		if (!initiateGUI) gui.startConfiguration();
 		else gui.refreshRulesList();
 		
@@ -35,19 +48,8 @@ public class AntiSpamFilterManualConfiguration {
 		
 		initiateGUI = true;
 	}
-
-	public ArrayList<String> getListOfNames() {
-		return listOfNames;
-	}
-
-	public void setWindowClose() {
-		main.configureWindowClose();
-	}
-
-	public Double getRuleWeight(int selectedIndex) {
-		return temporaryListOfRules.get(selectedIndex).getWeight();
-	}
 	
+	/** Returns the weight of a rule by the name **/
 	public Double getRuleWeight(String name) {
 		for (int i = 0; i < temporaryListOfRules.size(); i++)
 			if (temporaryListOfRules.get(i).getName().equals(name))
@@ -55,7 +57,8 @@ public class AntiSpamFilterManualConfiguration {
 		
 		return 0.0;
 	}
-
+	
+	/** Apply a certain weight value to a rule **/
 	public void applyWeightValue(String selectedRule, Double value) {
 		for (int i = 0; i < temporaryListOfRules.size(); i++)
 			if (temporaryListOfRules.get(i).getName().equals(selectedRule))
@@ -65,6 +68,7 @@ public class AntiSpamFilterManualConfiguration {
 		gui.refreshRulesList();
 	}
 	
+	/** Updates the list of rule's name with the temporary list **/
 	public void updateListOfNames() {
 		listOfNames = new ArrayList<String>();
 
@@ -74,6 +78,7 @@ public class AntiSpamFilterManualConfiguration {
 			else listOfNames.add("> " + mainListOfRules.get(i).getName());
 	}
 	
+	/** Resets a rule value to his original value **/
 	public void resetWeightValue(String name) {
 		if (name.charAt(0) == '>')
 			name = name.substring(2);
@@ -86,6 +91,7 @@ public class AntiSpamFilterManualConfiguration {
 		gui.refreshRulesList();
 	}
 
+	/** Apply a filter to the list of rules based in a text **/
 	public void filterRulesList(String text) {
 		if (text.isEmpty()) updateListOfNames();
 		else {
@@ -100,6 +106,7 @@ public class AntiSpamFilterManualConfiguration {
 		gui.refreshRulesList();
 	}
 	
+	/** Save the temporary list of rules in the main class **/
 	public void saveMainListOfRules() {
 		for (int i = 0; i < mainListOfRules.size(); i++)
 			mainListOfRules.get(i).setWeight(temporaryListOfRules.get(i).getWeight());
@@ -107,11 +114,25 @@ public class AntiSpamFilterManualConfiguration {
 		main.saveListOfRules(mainListOfRules);
 	}
 
+	/** Verifies if a list was changed **/
 	public boolean isListChanged() {
 		for (int i = 0; i < temporaryListOfRules.size(); i++)
 			if (temporaryListOfRules.get(i).getWeight() != mainListOfRules.get(i).getWeight())
 				return true;
 			
 		return false;
+	}
+	
+	public ArrayList<String> getListOfNames() {
+		return listOfNames;
+	}
+
+	/** Closes the window **/
+	public void setWindowClose() {
+		main.configureWindowClose();
+	}
+
+	public Double getRuleWeight(int selectedIndex) {
+		return temporaryListOfRules.get(selectedIndex).getWeight();
 	}
 }
--- src/antiSpamFilter/rules/RuleStream.java
+++ src/antiSpamFilter/rules/RuleStream.java
@@ -1,18 +1,12 @@
 package antiSpamFilter.rules;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
-
-import antiSpamFilter.validations.ReadCF;
+import antiSpamFilter.GUI.AntiSpamFilterStyles.AOptionPane;
 
 /**
  * 
@@ -27,10 +21,6 @@ public class RuleStream {
 	/**
 	 *Attribute that keeps track of the Rules.cf file from where every Rule is created. 
 	 */
-	private File filename;
-	/**
-	 * 
-	 */
 	private ArrayList<Rule> ruleStream = new ArrayList<Rule>();
 	
 	/**
@@ -38,43 +28,31 @@ public class RuleStream {
 	 * @param filename file with the information of rules to be used
 	 */
 	public RuleStream(File filename) {
-		this.filename = filename;
-		ReadCF reader = new ReadCF();
-		
-		if(reader.read(filename)) {
-			//Start the reading		
-			Scanner scanner;
-			String line;
-			String [] tokens;
-			try {
-				scanner = new Scanner(filename);
-				while (scanner.hasNextLine()) {
-					line = scanner.nextLine();
-					tokens = line.split("\t");
-					/*for (int i=0; i<tokens.length; i++) {
-					 System.out.println(tokens[i]);
-					}*/
-					Rule rule = new Rule (tokens[0], 0.0);
-					if(tokens.length>1) {
-						rule.setWeight(Double.parseDouble(tokens[1]));
-					}
-					ruleStream.add(rule);
-				}
+		//Start the reading		
+		Scanner scanner;
+		String line;
+		String [] tokens;
+		try {
+			scanner = new Scanner(filename);
 			
-			} catch (FileNotFoundException e) {
-				JOptionPane.showMessageDialog(null, "Could not read the file.", "Error", JOptionPane.ERROR_MESSAGE);
-				e.printStackTrace();
-				return;
-			}
-			Collections.sort(ruleStream);
+			while (scanner.hasNextLine()) {
+				line = scanner.nextLine();
+				tokens = line.split("\t");
+				Rule rule = new Rule (tokens[0], 0.0);
 				
-		}
-		else {
-			JOptionPane.showMessageDialog(null, "Could not read the file.", "Error", JOptionPane.ERROR_MESSAGE);
+				if(tokens.length>1) {
+					rule.setWeight(Double.parseDouble(tokens[1]));
+				}
+				ruleStream.add(rule);
+			}
+
+		} catch (FileNotFoundException e) {
+			AOptionPane.showMessageDialog(null, "Could not read the file.", "Error", AOptionPane.ERROR_MESSAGE);
 			return;
 		}
+		Collections.sort(ruleStream);
 	}
-	
+
 	/**
 	 * Getter with the purpose to obtain the arraylist attribute of the RuleStream
 	 * @return returns the arraylist of Rules
@@ -82,20 +60,6 @@ public class RuleStream {
 	public ArrayList<Rule> getRuleStream() {
 		return ruleStream;
 	}
-	
-	/**
-	 * Test method to make sure the array of Rules is constructed correctly.
-	 * @param args
-	 */
-	/*public static void main(String[] args) {
-
-		RuleStream r = new RuleStream(new File("Files/rulesExpRodolfo.cf"));
-		
-		for(int i=0; i < r.ruleStream.size(); i++) {
-			System.out.println(r.ruleStream.get(i).toString());
-		}
-
-	}*/
 
 
 
