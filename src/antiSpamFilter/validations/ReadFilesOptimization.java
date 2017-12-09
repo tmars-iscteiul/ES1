afd02cc2e0690e60ca63ab72aaa694b6d8653821
Catarina Carriço <cdpco@iscte-iul.pt>
2017-12-09 18:43:25 +0000

    Criação da classe ReadFilesOptimization e javadoc da mesma.

---
+++ src/antiSpamFilter/validations/ReadFilesOptimization.java
@@ -0,0 +1,17 @@
+package antiSpamFilter.validations;
+
+public class ReadFilesOptimization {
+	/**
+	 * <p>ReadFilesOptimization -  </br>
+	 * </br>
+	 * 
+	 * Check the variables of the files "Anti Spam Study.tex", "HS.Boxplot.R" and "AntiSpamFilterProblem"
+	 * relatively to the optimization that are necessary to show the in the results area.
+	 * </p>
+	 * 
+	 * @author ES1-2017-LIGE-PL-102
+	 *
+	 */
+	
+	
+}
