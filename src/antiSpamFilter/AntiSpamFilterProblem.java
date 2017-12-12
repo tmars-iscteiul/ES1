0e0eabe357be6d670a130ee6d40a4ce7255a38bb
Rodolfo Arnaldo <rgnsa@iscte-iul.pt>
2017-12-12 15:13:26 +0000

    AntiSpam Problem definido

--- src/antiSpamFilter/AntiSpamFilterProblem.java
+++ src/antiSpamFilter/AntiSpamFilterProblem.java
@@ -6,49 +6,73 @@ import java.util.List;
 import org.uma.jmetal.problem.impl.AbstractDoubleProblem;
 import org.uma.jmetal.solution.DoubleSolution;
 
+import antiSpamFilter.emails.Email;
+
 public class AntiSpamFilterProblem extends AbstractDoubleProblem {
 
-	  public AntiSpamFilterProblem() {
-	    // 10 variables (anti-spam filter rules) by default 
-	    this(10);
-	  }
+	private ArrayList<Email> listOfEmailsSpam, listOfEmailsHam;
 
-	  public AntiSpamFilterProblem(Integer numberOfVariables) {
-	    setNumberOfVariables(numberOfVariables);
-	    setNumberOfObjectives(2);
-	    setName("AntiSpamFilterProblem");
-
-	    List<Double> lowerLimit = new ArrayList<>(getNumberOfVariables()) ;
-	    List<Double> upperLimit = new ArrayList<>(getNumberOfVariables()) ;
-
-	    for (int i = 0; i < getNumberOfVariables(); i++) {
-	      lowerLimit.add(-5.0);
-	      upperLimit.add(5.0);
-	    }
-
-	    setLowerLimit(lowerLimit);
-	    setUpperLimit(upperLimit);
-	  }
-
-	  public void evaluate(DoubleSolution solution){
-	    double aux, xi, xj;
-	    double[] fx = new double[getNumberOfObjectives()];
-	    double[] x = new double[getNumberOfVariables()];
-	    for (int i = 0; i < solution.getNumberOfVariables(); i++) {
-	      x[i] = solution.getVariableValue(i) ;
-	    }
-
-	    fx[0] = 0.0;
-	    for (int var = 0; var < solution.getNumberOfVariables() - 1; var++) {
-		  fx[0] += Math.abs(x[0]); // Example for testing
-	    }
-	    
-	    fx[1] = 0.0;
-	    for (int var = 0; var < solution.getNumberOfVariables(); var++) {
-	    	fx[1] += Math.abs(x[1]); // Example for testing
-	    }
-
-	    solution.setObjective(0, fx[0]);
-	    solution.setObjective(1, fx[1]);
-	  }
+	public AntiSpamFilterProblem() {
+		// 10 variables (anti-spam filter rules) by default
+		this(10);
 	}
+
+	public AntiSpamFilterProblem(Integer numberOfVariables) {
+		setNumberOfVariables(numberOfVariables);
+		setNumberOfObjectives(2);
+		setName("AntiSpamFilterProblem");
+
+		List<Double> lowerLimit = new ArrayList<>(getNumberOfVariables());
+		List<Double> upperLimit = new ArrayList<>(getNumberOfVariables());
+
+		for (int i = 0; i < getNumberOfVariables(); i++) {
+			lowerLimit.add(-5.0);
+			upperLimit.add(5.0);
+		}
+
+		setLowerLimit(lowerLimit);
+		setUpperLimit(upperLimit);
+	}
+
+	public void evaluate(DoubleSolution solution) {
+		double[] fx = new double[getNumberOfObjectives()];
+		double[] x = new double[getNumberOfVariables()];
+
+		for (int i = 0; i < solution.getNumberOfVariables(); i++) {
+			x[i] = solution.getVariableValue(i);
+		}
+
+		// FP - ham que foi classificado spam
+		fx[0] = 0.0;
+		for (Email email : listOfEmailsHam) {
+			double ruleWeight = 0.0;
+			for (Integer rulePos : email.getRulesList())
+				ruleWeight += x[rulePos];
+
+			if (ruleWeight >= 5)
+				fx[0]++;
+		}
+
+		// FN - spam que foi classificado ham
+		fx[1] = 0.0;
+		for (Email email : listOfEmailsSpam) {
+			double ruleWeight = 0.0;
+			for (Integer rulePos : email.getRulesList())
+				ruleWeight += x[rulePos];
+
+			if (ruleWeight < 5)
+				fx[1]++;
+		}
+
+		solution.setObjective(0, fx[0]);
+		solution.setObjective(1, fx[1]);
+	}
+
+	public void setListOfHam(ArrayList<Email> listOfEmailsHam) {
+		this.listOfEmailsHam = listOfEmailsHam;
+	}
+
+	public void setListOfSpam(ArrayList<Email> listOfEmailsSpam) {
+		this.listOfEmailsSpam = listOfEmailsSpam;
+	}
+}
