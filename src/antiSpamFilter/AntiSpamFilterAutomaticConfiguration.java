e25c51bc5dddcc53abf45ecab14788b23bd08693
Rodolfo Arnaldo <rgnsa@iscte-iul.pt>
2017-12-12 15:58:32 +0000

    AntiSpam Configuration iniciado

--- src/antiSpamFilter/AntiSpamFilterAutomaticConfiguration.java
+++ src/antiSpamFilter/AntiSpamFilterAutomaticConfiguration.java
@@ -12,92 +12,101 @@ import java.io.File;
 import java.util.ArrayList;
 
 /**
- * <p>AntiSpamFilterAutomaticConfiguration - the main class</br>
+ * <p>
+ * AntiSpamFilterAutomaticConfiguration - the main class</br>
  * </br>
- * The AntiSpamFilter Automatic Configuration creates the main environment
- * to automatically configure and optimize the spam classification. </p>
+ * The AntiSpamFilter Automatic Configuration creates the main environment to
+ * automatically configure and optimize the spam classification.
+ * </p>
  *
  * @author ES1-2017-LIGE-PL-102
  */
 
 public class AntiSpamFilterAutomaticConfiguration {
 	private boolean filesAreValidated = false, isRulesChanged = false;
-	
+
 	AntiSpamFilterGUI mainGUI;
 	AntiSpamFilterManualConfiguration manualConfigure;
-	
+
 	ArrayList<Email> listOfEmailsSpam, listOfEmailsHam;
 	ArrayList<Rule> listOfRules;
 
-	File spamFile, hamFile, rulesFile; 
+	File spamFile, hamFile, rulesFile;
 
 	public AntiSpamFilterAutomaticConfiguration() {
 		mainGUI = new AntiSpamFilterGUI(this);
 		manualConfigure = new AntiSpamFilterManualConfiguration(this);
 	}
- 
-	/** Confirms if the log and rules files are validated and calls the optimization class **/
+
+	/**
+	 * Confirms if the log and rules files are validated and calls the optimization
+	 * class
+	 **/
 	public void runOptimization() {
 		if (!filesAreValidated)
 			AntiSpamFilterOptimization.runOptimization();
 	}
 
-	/** Validates the three files that represent the spam log, ham log and rules list **/
+	/**
+	 * Validates the three files that represent the spam log, ham log and rules list
+	 **/
 	public boolean validateFilesAndBuildRulesAndEmails(File spamFile, File hamFile, File rulesFile) {
 		boolean isSameFiles = false;
-		
-		//Check if the files are the same since the last optimization
+
+		// Check if the files are the same since the last optimization
 		if (this.spamFile == spamFile && this.hamFile == hamFile && this.rulesFile == rulesFile)
 			isSameFiles = true;
-		
-		//Check if the files were already validated before
+
+		// Check if the files were already validated before
 		if (!filesAreValidated || !isSameFiles) {
 			this.spamFile = spamFile;
 			this.hamFile = hamFile;
 			this.rulesFile = rulesFile;
 
-			//Validation of the files
+			// Validation of the files
 			if (!validateFiles()) {
-				filesAreValidated = false; 
+				filesAreValidated = false;
 				return false;
 			}
 
-			//Creation of the list of rules and emails
+			// Creation of the list of rules and emails
 			if (!buildRulesAndEmails()) {
-				filesAreValidated = false; 
+				filesAreValidated = false;
 				return false;
 			}
 
 			filesAreValidated = true;
 		}
-		
+
 		return true;
 	}
 
 	/** Calls the reading and validation classes **/
-	private boolean validateFiles() {		
-		//Call the validation classes and validate
-		if(ReadLOG.readFile(hamFile) && ReadLOG.readFile(spamFile)&& ReadCF.read(rulesFile))
+	private boolean validateFiles() {
+		// Call the validation classes and validate
+		if (ReadLOG.readFile(hamFile) && ReadLOG.readFile(spamFile) && ReadCF.read(rulesFile))
 			return true;
-	
+
 		return false;
 	}
 
 	/** Builds the list of rules and the list of emails **/
 	private boolean buildRulesAndEmails() {
-		//Creation of the list of rules
+		// Creation of the list of rules
 		listOfRules = new RuleStream(rulesFile).getRuleStream();
-		
-		//Creation of the lists of email Spam and email Ham
+
+		// Creation of the lists of email Spam and email Ham
 		listOfEmailsSpam = EmailStream.getListOfEmailsFromFile(spamFile, listOfRules, Email.SPAM);
 		listOfEmailsHam = EmailStream.getListOfEmailsFromFile(hamFile, listOfRules, Email.HAM);
 
-		if (listOfEmailsSpam == null || listOfEmailsHam == null) return false;
-		if (listOfEmailsSpam.size() == 0 || listOfEmailsHam.size() == 0) return false;
+		if (listOfEmailsSpam == null || listOfEmailsHam == null)
+			return false;
+		if (listOfEmailsSpam.size() == 0 || listOfEmailsHam.size() == 0)
+			return false;
 
 		return true;
 	}
-	
+
 	public ArrayList<Email> getListOfEmailsSpam() {
 		return listOfEmailsSpam;
 	}
@@ -125,5 +134,9 @@ public class AntiSpamFilterAutomaticConfiguration {
 
 	public boolean isRulesChanged() {
 		return isRulesChanged;
+	}
+
+	protected int getNumberOfRules() {
+		return listOfRules.size();
 	}
 }
