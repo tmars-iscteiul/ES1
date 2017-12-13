8cfbdad473f1f85c796b43df2ba0afebc233962d
Catarina Carriço <cdpco@iscte-iul.pt>
2017-12-13 18:12:00 +0000

    Correção do javadoc da classe ReadFilesOptimization

--- src/antiSpamFilter/validations/ReadFilesOptimization.java
+++ src/antiSpamFilter/validations/ReadFilesOptimization.java
@@ -1,17 +1,44 @@
 package antiSpamFilter.validations;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class ReadFilesOptimization {
 	/**
 	 * <p>ReadFilesOptimization -  </br>
 	 * </br>
 	 * 
-	 * Check the variables of the files "Anti Spam Study.tex", "HS.Boxplot.R" and "AntiSpamFilterProblem"
-	 * relatively to the optimization that are necessary to show the in the results area.
+	 * Check the variables of the files "AntiSpamFilterProblem.NSGAII.rf" and "AntiSpamFilterProblem.NSGAII.rs" 
+	 * relatively to the optimization, choose the results that are better for mixed-use email boxes and after 
+	 * show that results on results window.
 	 * </p>
 	 * 
 	 * @author ES1-2017-LIGE-PL-102
 	 *
 	 */
+	
+	public static boolean readFile(File f){
+		
+		
+		try {
+			Scanner s = new Scanner(f);
+			
+			while (s.hasNextLine()) {
+				
+				String nextLine = s.nextLine();
+				
+				
+						
+			}
+			s.close();
+			
+		} catch (FileNotFoundException e) {
+			
+		}
+		return true;
+	}
 	
 	
 }
